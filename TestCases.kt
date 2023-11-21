d73824a6b793e79527f8b7795808214e4ca5a79c
Venom2281 <94124037+Venom2281@users.noreply.github.com>
2023-11-21 19:55:17 +0200

    Create TestCases.kt

---
+++ TestCases.kt
@@ -0,0 +1,42 @@
+data class Message(val userName: String = "", val userMessage: String = "")
+
+object TestCases {
+    fun getMessageInfo(userMessage: String): Int {
+        if (isValidMessage(userMessage)) {
+            return 1 // Користувач коректно ввів дані
+        }
+
+        if (!isValidMessage(userMessage)) {
+            return -1 // Текст не відповідає умові або умовам
+        }
+        return -4 // Добавил значение по умолчанию, если нет других совпадений
+    }
+    fun isValidMessage(userMessage: String): Boolean {
+        return userMessage.length in 1..1000 && userMessage.all { it.isLetter() || it.isWhitespace() }
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
+class MedicialChatTest {
+    @Test
+    fun testValidMessage() {
+        ActivityScenario.launch(MedicialChat::class.java).onActivity{ medicalchat ->
+            val res = medicalchat.getMessageInfo("Hi I want to be free")
+            assertEquals(1, res)
+        }
+    }
+    @Test
+    fun testInvalidMessage() {
+        ActivityScenario.launch(MedicialChat::class.java).onActivity{ medicalchat ->
+            val res = medicalchat.getMessageInfo("")
+            assertEquals(-1, res)
+        }
+    }
+    @Test
+    fun testInvalidMessage2() {
+        ActivityScenario.launch(MedicialChat::class.java).onActivity{ medicalchat ->
+            val res = medicalchat.getMessageInfo("")
+            assertEquals(-1, res)
+        }
+    }
+}
